32e0dfd884bac7c9b7fc9ad782d00314ba527d0d
Fritjof Büttner <mail@fritjofbuettner.com>
2022-08-07 22:51:07 +0200

    serve frontend from fastapi if build dir exists

--- api/main.py
+++ api/main.py
@@ -1,8 +1,20 @@
+from pathlib import Path
+
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/api")
 async def hello_world():
     return {"hello": "world"}
+
+
+try:
+    public = Path(__file__).parent.parent / '.output' / 'public'
+    print(public)
+    app.mount("/", StaticFiles(directory=str(public), html=True), name="frontend")
+    print("Frontend ready to serve at /")
+except RuntimeError as e:
+    print(f"Frontend cannot be served: {e}")
